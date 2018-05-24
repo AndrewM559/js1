10331edf8761aa48b60eeba391ddede9a2fca9e0
AndrewM559 <andrewmontelongo559@gmail.com>
2018-05-23 21:02:24 -0700

    day 2 practice

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,23 @@
 var myName = "Andrew";
 console.log(myName);
 
-var reason = "I want to learn the fundamentals of programming so that i can learn better web developement, and programming in other languages";
-console.log(reason);
+var name = "Andrew";
+
+let birthday = new Date("11.21.94") ;
+
+const goal = "make money";
+
+//console.log(name+birthday+goal);
+//console.log (birthday.
+
+let favMovie = "Drive";
+
+let favFood = "Spaghetti";
+
+const movieYear = new Date ("9.16.2011");
+
+const ingredients = ['beef', 'tomato sauce', 'noodles']
+
+alert(favFood+ingredients);
+
+confirm(favMovie+movieYear)
