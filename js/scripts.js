c1c14b08a510213a20d28c1e257a7690cb70fa8b
AndrewM559 <andrewmontelongo559@gmail.com>
2018-05-23 19:06:39 -0700

    Reason for taking javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,5 @@
 var myName = "Andrew";
 console.log(myName);
+
+var reason = "I want to learn the fundamentals of programming so that i can learn better web developement, and programming in other languages";
+console.log(reason);
