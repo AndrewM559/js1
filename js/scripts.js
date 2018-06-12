64501f73e9c94e846b29e2c38c0a007fc29f9a60
AndrewM559 <andrewmontelongo559@gmail.com>
2018-06-11 20:11:02 -0700

    takehome

--- js/scripts.js
+++ js/scripts.js
@@ -1,95 +1,18 @@
-// //functions
-// console.log("Hello world");
-//
-// function capitalCase(string) {//FUNCTION DEFINITION
-// let words = string.split(' ');
-//  words.forEach(word => {
-//    let letters =word.split(' ');
-//    let capped = letters[0].toUpperCase();
-//    word.splice(0,1, capped);
-//
-//  });
-//  return words.join(' ')
+// function powerOfTwo(number) {
+//   if(!isNaN(number)) {
+//     return number * number;
+//   } else {
+//     powerOfTwo(parseInt(Prompt("that wasnt a number! what number?", '2')));
+//   }
 // };
 //
-// //the arguemnets in a function have no value until called
-//
-// let newString = capitalCase("peter pipera");
-//
-// console.log(newString)
+// let answer = powerOfTwo(parseInt(prompt("what number do you want to square?", '2')));
+// alert(answer);
 
-// console.log ("hello")
-
-// function ageDog(years) {
-//   return years * 7;
-// };
-//
-// alert(ageDog(
-//   parseInt(
-//     prompt("How old is your dog?","3")
-//   )
-// ));
-//
-//
-// function LifeTimeSupply(age, amountPerDay) {
-//   let life = 80 - age;
-//
-//
-// }
-
-let todos = [];
-
-let todo1 = [prompt("what do you want to do first?") ,"Eat"];
-let todo2 = [prompt("what do you want to do second?") ,"Eat"];
-let todo3 = [prompt("what do you want to do second?") ,"Eat"];
-
-todos.push(todo1,todo2,todo3);
-
-for(let i =0; i<todos.length; i++) {
-  let timeline = prompt(`how many days will it take ${todos[i][0]} to get  done?`, "2");
-
-  timeline = !isNaN(parseInt(timeline)) ?
-  parseInt(timeline) : 2;
-
-  todos[i].unshift(timeline);
+function parseString(string) {
+  let parsedLetter = string.charAt(0).toUpperCase();
+  let stringArr = string.split('');
+  stringArr.splice(0,1,parseLetter);
+  string = stringArr.join('');
+  if(string.charAt(string.length - 1))
 }
-
-// console.log(todos)
-
-
-todos.sort();
-
-let longest = todos[0];
-
-let j = 0;
-while(j < todos.length) {
-  if(todos[j][0] > longest[0]) longest = todos[j];
-
-//   longest = todos[j][0] > longest[0] ? longest = todos[j] :
-// }
-
-  j++;
-}
-
-// console.log(longest)
-//
-//
-
-longest.push("this")
-
-let z = 0;
-do {
-  if(todos[z][todos[z].length = 1] !=='this') {
-    todos[z].push('done');
-  }
-  z++;
-}while(z < todos.length);
-
-let doneItems = [];
-
-todos.forEach(
-  todos => {
-    if(todos[todos.length - 1] === 'done') doneItems.push(todos);
-  });
-
-  alert(todos);
